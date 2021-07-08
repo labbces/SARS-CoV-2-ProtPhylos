4be60ba0bff0309a0fbb31f3e42f32d358521ddc
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2021-07-07 23:45:35 -0300

    updating extracting scripts

--- Scripts/Cluster/extracting_proteins_pathlib.py
+++ Scripts/Cluster/extracting_proteins_pathlib.py
@@ -1,40 +1,38 @@
-import os
 import argparse
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
+from pathlib import Path
+
 
 # Using argparse to improve program execution from terminal
 parser = argparse.ArgumentParser()
 parser.add_argument("input_Path", help="Path where the files with embl annotation are")
 parser.add_argument("output_Path", help="Path where SeqI0.write will write the files")
-parser.add_argument("Region", help="Region where the sequences come from")
 parser.add_argument("protein", help="Protein to be extracted")
 args = parser.parse_args()
 
 # Using path and region that was given how argument
 input_path = args.input_Path
-embl_path = os.scandir(input_path)
 output_path = args.output_Path
-region = args.Region
 protein_name = args.protein
+paths = Path(input_path).glob('**/*.embl')
 
 # List to store record from the sequences
 Gene_list = []
 Protein_list = []
 
 # file to save invalid files
-invalid_files = open(f'invalid_files_{protein_name}_{region}.txt', 'w')
+invalid_files = open(f'invalid_files_{protein_name}_global.txt', 'w')
 
 # creating variables to evaluate performance
 count1 = dict()
 count2 = 0
 
 # Making SeqRecords and storing in the lists
-for file in embl_path:
-    if file.path.endswith(".embl") and file.is_file():
+for path in paths:
         try:
             count2 += 1
-            for record in SeqIO.parse(file, "embl"):
+            for record in SeqIO.parse(path, "embl"):
                 for feature in record.features:
                     if feature.type == "gene" and feature.qualifiers["gene"][0] == f'{protein_name}':
                         position = feature.location
@@ -56,12 +54,12 @@ for file in embl_path:
                                 count1[element] += 1
 
         except:
-            if file.is_file():
-                invalid_files.write(file.path)
+            if path.is_file():
+                invalid_files.write(path)
 
 # Printing information about how many sequences actually work
 print(count1, count2)
 
 # Writing the multifasta files with the sequences
-SeqIO.write(Gene_list, f'{output_path}/{region}_gene_{protein_name}.fasta', "fasta")
-SeqIO.write(Protein_list, f'{output_path}/{region}_protein_{protein_name}.fasta', "fasta")
+SeqIO.write(Gene_list, f'{output_path}/global_gene_{protein_name}.fasta', "fasta")
+SeqIO.write(Protein_list, f'{output_path}/global_protein_{protein_name}.fasta', "fasta")
