9cbf0a9606762d7b8f48f86a7b58193e8dd6d0cc
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2021-07-28 18:34:57 -0300

    updating variety script

---
+++ Scripts/Cluster/full_variety.py
@@ -0,0 +1,47 @@
+from Bio import SeqIO
+import argparse
+
+
+# Function: Script to get IDs of sequences that represent the cluster obtained from CD-HTI
+
+# Using argparse to use script by terminal
+parser = argparse.ArgumentParser()
+parser.add_argument("IDsList", help="list of ids of cluster sequences obtained by CD-HIT(proteins)")
+parser.add_argument("MultiFaGenes", help="multifasta file with genes sequences")
+args = parser.parse_args()
+
+# parser.add_argument("clstr", help="file clstr, output from cd-hit")
+# parser.add_argument("variety_gene", help="genes corresponding to proteins that represent cluster")
+# parser.add_argument("protein_reformated", help="file with cluster proteins, but using duplication when necessary")
+# alinhamento é o arquivo de saída
+
+try:
+    handle_id = open(args.IDsList, "r")
+except:
+    print("File", args.IDsList, "cannot be open")
+    exit()
+
+try:
+    handle_multifa = open(args.MultiFaGenes, "r")
+except:
+    print("File", args.MultiFaGenes, "cannot be open")
+    exit()
+
+
+ids = SeqIO.parse(handle_id, "fasta")
+multifa = SeqIO.parse(handle_multifa, "fasta")
+
+gisaid_ids = dict()
+
+for identifier in ids:
+    protein_id = identifier.id.split("__")[1]
+    gisaid_ids[protein_id] = 0
+
+my_records = list()
+
+for identifier in multifa:
+    gene_id = identifier.id.split("__")[1]
+    if gene_id in gisaid_ids:
+        my_records.append(identifier)
+
+SeqIO.write(my_records, "clusterGenes.fasta", "fasta")
